c19e2b0c81bc5a6825b76e253df9a899ab7ce287
LukasScholz <lukas.scholz@deutschebahn.com>
2022-11-29 10:28:01 +0100

    added experiment

---
+++ controllers/hoap2/build/release/hoap2.d
@@ -0,0 +1,8 @@
+build/release/hoap2.o: hoap2.c \
+  /Applications/Webots.app/Contents/include/controller/c/webots/emitter.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/types.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/motor.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/position_sensor.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/robot.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/nodes.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/touch_sensor.h
